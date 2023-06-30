fa85d9b89c468703bde3a3077dfff0ac2d0686af
+ <+>
2023-06-29 22:11:19 -0400

    trainers list and populated it with the seed data

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -5,7 +5,6 @@ import random
 
 from models import Member, Trainer, Exercise
 
-fake = Faker()
 
 if __name__ == "__main__":
     engine = create_engine("sqlite:///the_muscle_factory.db")
@@ -15,4 +14,24 @@ if __name__ == "__main__":
     session.query(Member).delete()
     session.query(Trainer).delete()
     session.query(Exercise).delete()
+
+    fake = Faker()
+    
+    trainers = []
+
+    for _ in range(5):
+        trainer = Trainer(
+            first_name=f"{fake.first_name()}",
+            last_name=f"{fake.last_name()}",
+            years_of_experience=random.randint(3, 10),
+        )
+        trainers.append(trainer)
+        session.add(trainer)
+        session.commit()
+
+    
+
+  
+    
+
   
