07ac071b693394d479c21c68188a28316e19a6b5
+ <+>
2023-06-29 21:23:43 -0400

    created exercise class and added one to many relationship between trainer and exercise

--- lib/db/models.py
+++ lib/db/models.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import relationship, backref
 Base = declarative_base()
 
 class Trainer(Base):
+
     __tablename__ = 'trainers'
 
     id = Column(Integer(), primary_key=True)
@@ -12,6 +13,7 @@ class Trainer(Base):
     last_name = Column(String())
     years_of_experience = Column(Integer())
     members = relationship("Member", backref="trainer")
+    exercises = relationship("Exercise", backref="trainer")
 
     def __repr__(self):
         return (
@@ -21,23 +23,34 @@ class Trainer(Base):
         )
     
 
-    class Member(Base):
-        __tablename__ = 'members'
+class Member(Base):
 
-        id = Column(Integer(), primary_key=True)
-        first_name = Column(String())
-        last_name = Column(String())
-        gym_goal = Column(String())
-        trainings_per_week = Column(Integer())
-        trainer_id = Column(Integer(), ForeignKey('trainers.id'))
+    __tablename__ = 'members'
+
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String())
+    last_name = Column(String())
+    gym_goal = Column(String())
+    trainings_per_week = Column(Integer())
+    trainer_id = Column(Integer(), ForeignKey('trainers.id'))
 
 
 
-        def __repr__(self):
-            return (
-                f"Id: {self.id}, "
-                + f"Member's Name: {self.first_name} {self.last_name}, "
-                + f"Gym Goal: {self.gym_goal}, "
-                + f"Trainings per week: {self.trainings_per_week}, "
-            )
+    def __repr__(self):
+        return (
+            f"Id: {self.id}, "
+            + f"Member's Name: {self.first_name} {self.last_name}, "
+            + f"Gym Goal: {self.gym_goal}, "
+            + f"Trainings per week: {self.trainings_per_week}, "
+        )
+    
+class Exercise():
+
+    __tablename__= "exercises"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    intensity = Column(String())
+    durations = Column(Integer())
+    trainer_id = Column(Integer(), ForeignKey('trainers.id'))
 
