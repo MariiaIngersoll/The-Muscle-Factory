eed8f468afcbf9b076a729e2bb2da814dbacc8bf
+ <+>
2023-07-02 14:24:53 -0400

    fixed an error that kept saying that trainers table do not exist

--- lib/db/debug.py
+++ lib/db/debug.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from models import (Trainer, Member, Exercise)
 
 if __name__ == '__main__':
-    engine = create_engine("sqlite:///the_muscle_factory.db")
+    engine = create_engine("sqlite:///muscle_factory.db")
     session = Session(engine, future=True)
 
     import ipdb; ipdb.set_trace()
