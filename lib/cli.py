1c8e5a91e35163dbdbb8cd5b8249cf0d1d72b5fe
+ <+>
2023-07-04 22:51:21 -0400

    Created 5 options for my user menu

--- lib/cli.py
+++ lib/cli.py
@@ -2,28 +2,44 @@
 import importlib
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from db.models import Member
+from db.models import Member, Trainer, Exercise
 
 engine = create_engine("sqlite:///db/muscle_factory.db")
 session = Session(engine, future=True)
 
-# sys.path.append('./helper_functions')
 
-# user is able to find trainsers based on their goals. 
 def reroute():
     main()
 
 def main():
-    print("Hello user! ")
+    print(f"""
+                        Hello user!
+            Welcome to The Muscle Factory Gym!""")
     choice = 0
     while choice !=5:
         print(f'''
-          What would like to do today?
-          1 - Sign up for a membership
-          4 - Exit
-           ''')
+            What would like to do today?
+            1 - I would like to know what kind of exercises and gym classes I can attend as a member of this gym?
+            
+            2 - Sign up for a membership
+              
+            3 - Edit my information
+              
+            4 - Cancel my membership
+              
+            5 - Exit
+            ''')
         choice = int(input())
         if choice == 1:
+            print("Sure! Pick a class you are the most interested in! We got great options!")
+            all = session.query(Exercise).all()
+            for i in all:
+                print(i)
+                
+            trainer_id = int(input("Select an name of a class and we will assign you a trainer!"))
+            # how to assign a class to a specific trainer?
+
+        if choice == 2:
             print("Lets Sign Up!")
             first_name = input("What is your name?")
             last_name = input("What is your last name?")
@@ -36,10 +52,13 @@ def main():
                         trainings_per_week= trainings_per_week)
             session.add(new)
             session.commit()
-            print("WELCOME!")
+            print("Welcome to the Muscle Factory gym. Hope you enjoy your time here!!!")
 
             reroute()
-        if choice == 4:
+
+       
+
+        if choice == 5:
             print("Goodbye")
             break
 
